dd39be25a82d06b961d5f8e06857a83aa29b8f39
Aleksandr <beloovsasha23@gmail.com>
2025-02-02 23:03:13 -0800

    Add files via upload

---
+++ lesailes/funtions.py
@@ -0,0 +1,20 @@
+
+import aiogram
+from aiogram import types, Bot, Dispatcher
+from aiogram.filters import Command
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, InputMedia, WebAppInfo, message
+import asyncio
+from random import randint
+
+from pyexpat.errors import messages
+
+from funtions import *
+
+from russian import handle_text_r
+
+TOKEN = "7702041998:AAGYq7SjeufGnJQ4OFAr2zrzqVP7VyD6nCM"
+
+
+
+from lesailesbot import *
+
